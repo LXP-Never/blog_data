d9e7d7aeb321d499db0d314f3a627642b0807b07
凌逆战 <40896425+LXP-Never@users.noreply.github.com>
2020-04-15 16:48:26 +0800

    Delete 111.py

--- deep Dream/111.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-k = np.float32([1, 4, 6, 4, 1])
-k = np.outer(k, k)  # 计算两个向量的外积(5, 5)
-k5x5 = k[:, :, None, None] / k.sum() * np.eye(3, dtype=np.float32)
-print(k5x5.shape)
-print(k5x5)     # (5, 5, 3, 3)
